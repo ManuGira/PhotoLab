f3a29186917c8783f87f8daa09ae7390a4a20192
Emmanuel Girardin <emmanuel.girardin@outlook.com>
2024-04-19 14:21:19 +0200

    add color_spaces.py

---
+++ photolab/color_spaces.py
@@ -0,0 +1,230 @@
+import numpy as np
+import cv2
+import enum
+import numba
+
+# https://en.wikipedia.org/wiki/LMS_color_space
+HuntPointerEstevezMatrix = np.array([
+    [0.38971, 0.68898, -0.07868],  # L
+    [-0.22981, 1.18340, 0.04641],  # M
+    [0, 0, 1],  # S
+])
+HuntPointerEstevezMatrix /= np.sum(HuntPointerEstevezMatrix, axis=1)
+
+
+def apply_color_matrix(img, mat3x3):
+    h, w = img.shape[:2]
+    return (img.reshape(-1, 3) @ mat3x3.T).reshape(h, w, 3)
+
+
+def BGR_to_RGB_u8(bgr):
+    assert bgr.dtype == np.uint8
+    mat = np.array([
+        [0, 0, 1],
+        [0, 1, 0],
+        [1, 0, 0],
+    ], dtype=np.uint8)
+    res = apply_color_matrix(bgr, mat)
+    assert res.dtype == np.uint8
+    return res
+
+
+def RGB_to_BGR_u8(rgb):
+    assert rgb.dtype == np.uint8
+    bgr = BGR_to_RGB_u8(rgb)
+    assert bgr.dtype == np.uint8
+    return bgr
+
+
+def BGR_to_XYZ_u8(bgr):
+    assert bgr.dtype == np.uint8
+    res = cv2.cvtColor(bgr, cv2.COLOR_BGR2XYZ)
+    assert res.dtype == np.uint8
+    return res
+
+def XYZ_to_BGR_u8(xyz):
+    assert xyz.dtype == np.uint8
+    bgr = cv2.cvtColor(xyz, cv2.COLOR_XYZ2BGR)
+    assert bgr.dtype == np.uint8
+    return bgr
+
+
+def BRG_to_HLScube_u8(bgr):
+    assert bgr.dtype == np.uint8
+    hls = cv2.cvtColor(bgr, cv2.COLOR_BGR2HLS)
+    hls[:, :, 0] = (hls[:, :, 0].astype(np.float16) * (255 / 180)).astype(np.uint8)
+    assert hls.dtype == np.uint8
+    return hls
+
+
+def HLScube_to_BGR_u8(hls):
+    assert hls.dtype == np.uint8
+    hls[:, :, 0] = (hls[:, :, 0].astype(np.float16)*(180/255)).astype(np.uint8)
+    bgr = cv2.cvtColor(hls, cv2.COLOR_HLS2BGR)
+    assert bgr.dtype == np.uint8
+    return bgr
+
+def BRG_to_HSVcube_u8(bgr):
+    assert bgr.dtype == np.uint8
+    hsv = cv2.cvtColor(bgr, cv2.COLOR_BGR2HSV)
+    hsv[:, :, 0] = (hsv[:, :, 0].astype(np.float16) * (255 / 180)).astype(np.uint8)
+    assert hsv.dtype == np.uint8
+    return hsv
+
+def HSVcube_to_BGR_u8(hsv):
+    assert hsv.dtype == np.uint8
+    hsv[:, :, 0] = (hsv[:, :, 0].astype(np.float16)*(180/255)).astype(np.uint8)
+    bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    assert bgr.dtype == np.uint8
+    return bgr
+
+
+class ColorSpace(enum.IntEnum):
+    BGR = enum.auto()
+    RGB = enum.auto()
+    HLS = enum.auto()
+    HSV = enum.auto()
+    XYZ = enum.auto()
+    LAB = enum.auto()
+    LMS = enum.auto()
+
+
+CHANNELS = {
+    ColorSpace.BGR: ("Blue", "Green", "Red"),
+    ColorSpace.RGB: ("Red", "Green", "Blue"),
+    ColorSpace.HLS: ("Hue", "Light", "Saturation"),
+    ColorSpace.HSV: ("Hue", "Saturation", "Value"),
+    ColorSpace.XYZ: ("X", "Y", "Z"),
+    ColorSpace.LAB: ("L", "A", "B"),
+    ColorSpace.LMS: ("Long", "Medium", "Short"),
+}
+
+def convert_to_BGR_u8(img, from_space: ColorSpace):
+    match from_space:
+        case ColorSpace.BGR:
+            return img
+        case ColorSpace.RGB:
+            return RGB_to_BGR_u8(img)
+        case ColorSpace.XYZ:
+            return XYZ_to_BGR_u8(img)
+        case ColorSpace.HLS:
+            return HLScube_to_BGR_u8(img)
+        case ColorSpace.HSV:
+            return HSVcube_to_BGR_u8(img)
+
+
+def convert_from_BGR_u8(bgr, to_space: ColorSpace):
+    match to_space:
+        case ColorSpace.BGR:
+            return bgr
+        case ColorSpace.RGB:
+            return BGR_to_RGB_u8(bgr)
+        case ColorSpace.XYZ:
+            return BGR_to_XYZ_u8(bgr)
+        case ColorSpace.HLS:
+            return BRG_to_HLScube_u8(bgr)
+        case ColorSpace.HSV:
+            return BRG_to_HSVcube_u8(bgr)
+
+
+class ColorImage:
+    def __init__(self, img, space: ColorSpace):
+        self.img = convert_to_BGR_u8(img, from_space=space)
+
+    def to(self, space: ColorSpace = ColorSpace.BGR):
+        return convert_from_BGR_u8(self.img, to_space=space)
+
+
+@numba.njit()
+def apply_lut_numba(lut, img):
+    res = np.zeros_like(img)
+    h, w = img.shape[:2]
+    for y in range(h):
+        for x in range(w):
+            for c in range(3):
+                res[y, x, c] = lut[img[y, x, 0], img[y, x, 1], img[y, x, 2], c]
+    return res
+
+
+class LUTConverter:
+    """
+    Experimental function that precompute a 255x255x255 look up table to convert between different color spaces.
+    It is 10x slower than the opencv functions even when using numba
+    """
+
+    def __init__(self, from_space: ColorSpace, to_space: ColorSpace):
+        r256 = np.arange(256, dtype=np.uint8)
+        x, y, z = np.meshgrid(r256, r256, r256, indexing="ij")
+        self.lut = np.concatenate((x.reshape(-1, 1, 1), y.reshape(-1, 1, 1), z.reshape(-1, 1, 1)), axis=2)
+        self.lut = ColorImage(self.lut, from_space).to(to_space)
+        self.lut = self.lut.reshape(256, 256, 256, 3)
+        self.apply_lut_fast(np.zeros((1, 1, 1), dtype=np.uint8))
+
+    def apply_lut_fast(self, img):
+        return apply_lut_numba(self.lut, img)
+
+    def apply_lut(self, img):
+        return self.lut[img[:, :, 0], img[:, :, 1], img[:, :, 2]]
+
+
+#
+# 
+
+# 
+# 
+# def BGR_to_LMS_u8(bgr):
+#     xyz = cv2.cvtColor(bgr, cv2.COLOR_BGR2XYZ).astype(float) / 255
+#     return apply_color_matrix(xyz, HuntPointerEstevezMatrix)
+# 
+# 
+# def LMS_to_BGR_u8(lms):
+#     xyz = apply_color_matrix(lms, np.linalg.inv(HuntPointerEstevezMatrix)) * 255
+#     return cv2.cvtColor(xyz.astype(np.uint8), cv2.COLOR_XYZ2BGR)
+#
+
+def main():
+    import time
+    import guibbon as gbn
+    img = cv2.imread("../images/baboon_512x512.png")
+    csc = LUTConverter(ColorSpace.BGR, ColorSpace.XYZ)
+    gbn.imshow("ColorConversion", img)
+    gbn.waitKeyEx(0)
+    gbn.imshow("ColorConversion", cv2.cvtColor(img.copy(), cv2.COLOR_BGR2XYZ))
+    gbn.waitKeyEx(0)
+    gbn.imshow("ColorConversion", csc.apply_lut(img.copy()))
+    gbn.waitKeyEx(0)
+
+    def fps(dt):
+        return 100 / (tock - tick)
+
+    print("Time cv2: ", end="")
+    tick = time.time()
+    for k in range(100):
+        img2 = cv2.cvtColor(img, cv2.COLOR_BGR2XYZ)
+    tock = time.time()
+    print(fps(tock - tick))
+
+    print("Time mat mul: ", end="")
+    tick = time.time()
+    for k in range(100):
+        img2 = BGR_to_RGB_u8(img)
+    tock = time.time()
+    print(fps(tock - tick))
+
+    print("Time numba lut: ", end="")
+    tick = time.time()
+    for k in range(100):
+        img2 = apply_lut_numba(csc.lut, img)
+    tock = time.time()
+    print(fps(tock - tick))
+
+    print("Time np lut: ", end="")
+    tick = time.time()
+    for k in range(100):
+        img2 = csc.apply_lut(img)
+    tock = time.time()
+    print(fps(tock - tick))
+
+
+if __name__ == '__main__':
+    main()
--- scripts/main_color_spaces.py
+++ scripts/main_color_spaces.py
@@ -3,30 +3,8 @@ import enum
 import cv2
 import guibbon as gbn
 import numpy as np
-
-# https://en.wikipedia.org/wiki/LMS_color_space
-HuntPointerEstevezMatrix = np.array([
-    [0.38971, 0.68898, -0.07868],  # L
-    [-0.22981, 1.18340, 0.04641],  # M
-    [0, 0, 1],  # S
-])
-
-HuntPointerEstevezMatrix /= np.sum(HuntPointerEstevezMatrix, axis=1)
-
-
-def apply_color_matrix(img, mat3x3):
-    h, w = img.shape[:2]
-    return (img.reshape(-1, 3) @ mat3x3.T).reshape(h, w, 3)
-
-
-def cvtBGR_to_LMS(bgr):
-    xyz = cv2.cvtColor(bgr, cv2.COLOR_BGR2XYZ).astype(float) / 255
-    return apply_color_matrix(xyz, HuntPointerEstevezMatrix)
-
-
-def cvtLMS_to_BGR(lms):
-    xyz = apply_color_matrix(lms, np.linalg.inv(HuntPointerEstevezMatrix)) * 255
-    return cv2.cvtColor(xyz.astype(np.uint8), cv2.COLOR_XYZ2BGR)
+from photolab import utils as ut
+from photolab import color_spaces as cs
 
 
 class ColorSpaceApp:
@@ -36,13 +14,28 @@ class ColorSpaceApp:
         channel0: int = 0
         channel1: int = 0
         channel2: int = 0
+        color_space = cs.ColorSpace.BGR
 
     def __init__(self):
         self.win = gbn.create_window("Color Spaces")
         self.config = ColorSpaceApp.Config()
-        self.win.create_slider("Blue", range(255), self.on_change_channel0, initial_index=self.config.channel0)
-        self.win.create_slider("Green", range(255), self.on_change_channel1, initial_index=self.config.channel1)
-        self.win.create_slider("Red", range(255), self.on_change_channel2, initial_index=self.config.channel2)
+
+        self.color_space_list = [cs.ColorSpace.BGR, cs.ColorSpace.RGB, cs.ColorSpace.XYZ, cs.ColorSpace.HLS, cs.ColorSpace.HSV]
+        self.win.create_radio_buttons("Color Space", [str(space).split(".")[-1] for space in self.color_space_list], self.on_change_color_space)
+
+        self.slider_c0 = self.win.create_slider("00", range(255), self.on_change_channel0, initial_index=self.config.channel0)
+        self.slider_c1 = self.win.create_slider("00", range(255), self.on_change_channel1, initial_index=self.config.channel1)
+        self.slider_c2 = self.win.create_slider("00", range(255), self.on_change_channel2, initial_index=self.config.channel2)
+
+        channel_names = cs.CHANNELS[self.config.color_space]
+        self.slider_c0.name = channel_names[0]
+        self.slider_c1.name = channel_names[1]
+        self.slider_c2.name = channel_names[2]
+
+        # self.slider_c0.name.set(channel_names[0])
+        # self.slider_c1.name.set(channel_names[1])
+        # self.slider_c2.name.set(channel_names[2])
+
         self.need_update = True
 
     def run(self):
@@ -54,7 +47,9 @@ class ColorSpaceApp:
             self.win.imshow(self.result)
             self.win.waitKeyEx(1)
 
+
     def update(self):
+
         c0 = self.config.channel0
         c1 = self.config.channel1
         c2 = self.config.channel2
@@ -72,23 +67,27 @@ class ColorSpaceApp:
         img_12 = np.concatenate((zs + c0, xs, ys), axis=2)
         img[N:0:-1, N:] = img_12
 
-        img[N+N//4:N+(3*N)//4, N//4:(3*N)//4, :] = np.array([[[c0, c1, c2]]])
+        img = cs.ColorImage(img, self.config.color_space).to(cs.ColorSpace.BGR)
 
+        # color square
+        img[N + N // 4:N + (3 * N) // 4, N // 4:(3 * N) // 4, :] = np.array([[[c0, c1, c2]]])
+
+        # cursors
         img[:, N + c1, :] = 255
         img[N - c2, :, :] = 255
-        img[N+c0, N:, :] = 255
-        img[:N, N-c0, :] = 255
+        img[N + c0, N:, :] = 255
+        img[:N, N - c0, :] = 255
 
         img[N, :, :] = 0
         img[:, N, :] = 0
         return img
 
-    def on_change_filename(self, ind, val):
-        self.config.filename = val
-        self.need_update = True
-
-    def on_change_filter_type(self, ind, val):
-        self.config.filter_type = val
+    def on_change_color_space(self, ind: int, val: str):
+        self.config.color_space = self.color_space_list[ind]
+        channel_names = cs.CHANNELS[self.config.color_space]
+        self.slider_c0.name = channel_names[0]
+        self.slider_c1.name = channel_names[1]
+        self.slider_c2.name = channel_names[2]
         self.need_update = True
 
     def on_change_channel0(self, ind, val):
