16ae554a5f9d5416e1cee05b8b3d95ac88a00b4e
Emmanuel Girardin <emmanuel.girardin@outlook.com>
2024-09-02 22:44:03 +0200

    ADD(DitherArt): vert, horiz and diag matrices - chromatic abberation - easy_save

--- scripts/dither/dither_matrix.py
+++ scripts/dither/dither_matrix.py
@@ -1,5 +1,26 @@
 import numpy as np
 
+
+def prime_factorization(x):
+    for i in range(2, x):
+        if (x // i) * i == x:
+            return [i] + prime_factorization(x // i)
+            break
+    return [x]
+
+
+def find_smaller_coprimes(x):
+    """Find and return the list of all numbers coprimes with x and smaller than x"""
+    coprimes = []
+    x_factors = set(prime_factorization(x))
+    for i in range(2, x):
+        i_factors = set(prime_factorization(i))
+        if len(i_factors.intersection(x_factors)) == 0:
+            coprimes.append(i)
+
+    return coprimes
+
+
 M2_classic = np.array([
     [0, 2],
     [3, 1],
@@ -43,17 +64,59 @@ def hamming(N=8):
     return res
 
 
-def diag(N=8):
+def vert(N=8):
+    assert N > 1
+    best_c = 1
+
+    if N > 2:
+        coprimes = find_smaller_coprimes(N)
+
+        golden_ratio = (5 ** 0.5 - 1) / 2
+        smaller_dist = N
+        for c in coprimes:
+            dist = abs(c - golden_ratio * N)
+            if dist < smaller_dist:
+                smaller_dist = dist
+                best_c = c
+
     xs, ys = np.meshgrid(range(N), range(N))
+    xs = np.mod(xs * best_c, N)
+
     xs = 2 * xs / (N - 1)
     ys = 2 * ys / (N - 1)
-    weights = abs(xs) + abs(ys)/1000
+
+    weights = abs(xs) + abs(ys) / 1000
     res = get_order(weights)
+    return res
+
+
+def horiz(N=8):
+    res = vert(N)
+    res = res.T
+    return res
+
+
+def diag(N=8):
+    res = vert(N)
+
+    for i in range(N):
+        res[i, :] = np.roll(res[i, :], i)
+
     return res
 
 
 def random(N=8):
     return np.random.permutation(N ** 2).reshape((N, N))
+
+
+def main():
+    import matplotlib.pyplot as plt
+    plt.imshow(diag(8))
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
 
 # def get_matrix_by_name(name):
 #     match name:
--- scripts/dither/main_dither.py
+++ scripts/dither/main_dither.py
@@ -5,6 +5,22 @@ import cv2
 import guibbon as gbn
 import photolab.utils
 import dither_matrix
+import os
+
+
+def easy_save(img, filename):
+    dir = os.path.dirname(filename)
+    filename_no_ext, ext = os.path.splitext(filename)
+
+    os.makedirs(dir, exist_ok=True)
+
+    n = 1
+    while os.path.exists(filename):
+        filename = f"{filename_no_ext}-{n:03}{ext}"
+        n += 1
+
+    cv2.imwrite(filename, img)
+
 
 
 def apply_dither_matrx(img, mat):
@@ -22,10 +38,12 @@ def apply_dither_matrx(img, mat):
 
 def compute_dither_art(dither_mat, height: int = 200, gain_db: float = 0):
     img = cv2.imread("../../images/david_656x457.jpg", cv2.IMREAD_GRAYSCALE)
+    # img = cv2.imread("../../images/ae86_563x1000.jpg", cv2.IMREAD_GRAYSCALE)
+    # img = cv2.imread("../../images/ae86_543x1000.png", cv2.IMREAD_GRAYSCALE)
+    # img = cv2.imread("../../images/sphere_415x415.png", cv2.IMREAD_GRAYSCALE)
     img = photolab.utils.resize(img, new_height=height)
 
     img = np.clip(0, 255, img.astype(float) * 2 ** gain_db).astype(np.uint8)
-
 
     n = len(dither_mat.flatten())
     dither_mat = ((dither_mat + 1).astype(float) * 255 / (n + 1)).astype(np.uint8)
@@ -41,9 +59,11 @@ class DitherArt:
         gain: float = 0
         dither_matrix_type: str = "hamming"
         dither_matrix_size: int = 0
+        abberation: int = 0
 
     def __init__(self):
         self.winname = "Dither Art"
+        self.savename = "out/dither.png"
         win: gbn.Guibbon = gbn.create_window(self.winname)
         height_range = (200 * 2 ** np.arange(-2, 3, dtype=float)).astype(int)
         height_slider = win.create_slider("res", height_range, self.onchange_height_slider, initial_index=len(height_range) // 2)
@@ -54,13 +74,26 @@ class DitherArt:
 
         size_slider = win.create_slider("matrix size", range(2, 10), self.onchange_size_slider, initial_index=0)
 
-        matrix_type_options = win.create_radio_buttons("matrix type", ["classic", "hamming", "spiral", "random", "diag"], self.onchange_dithermatrixtype)
+        matrix_type_options = win.create_radio_buttons("matrix type", [
+            "classic",
+            "hamming",
+            "spiral",
+            "random",
+            "vert",
+            "diag",
+            "horiz",
+        ], self.onchange_dithermatrixtype)
+
+        abberation_slider = win.create_slider("abberation", range(-15, 15), self.onchange_abberation_slider, initial_index=15)
+
+        win.create_button("Save", self.onclick_save)
 
         self.config = self.Config(
             height=height_slider.get_values()[height_slider.get_index()],
             gain=float(gain_slider.get_values()[gain_slider.get_index()]),
             dither_matrix_size=size_slider.get_values()[size_slider.get_index()],
-            dither_matrix_type=matrix_type_options.get_current_selection()[1]
+            dither_matrix_type=matrix_type_options.get_current_selection()[1],
+            abberation=abberation_slider.get_values()[abberation_slider.get_index()],
         )
 
         self.result: Image_t
@@ -82,6 +115,12 @@ class DitherArt:
         self.config.dither_matrix_size = val
         self.is_update_needed = True
 
+    def onchange_abberation_slider(self, i, val):
+        self.config.abberation = val
+        self.is_update_needed = True
+
+    def onclick_save(self):
+        easy_save(self.result, self.savename)
 
     def update(self):
         match self.config.dither_matrix_type:
@@ -91,13 +130,23 @@ class DitherArt:
                 mat = dither_matrix.hamming(N=self.config.dither_matrix_size)
             case "random":
                 mat = dither_matrix.random(N=self.config.dither_matrix_size)
+            case "vert":
+                mat = dither_matrix.vert(N=self.config.dither_matrix_size)
             case "diag":
                 mat = dither_matrix.diag(N=self.config.dither_matrix_size)
+            case "horiz":
+                mat = dither_matrix.horiz(N=self.config.dither_matrix_size)
             case _:
                 mat = dither_matrix.M2_classic
 
+        dither = compute_dither_art(mat, self.config.height, self.config.gain)
 
-        self.result = compute_dither_art(mat, self.config.height, self.config.gain)
+        self.result = np.repeat(np.zeros_like(dither, shape=dither.shape + (1,)), repeats=3, axis=2)
+        shift_b = (self.config.abberation + 1) // 2
+        shift_r = self.config.abberation // 2
+        self.result[:, :, 0] = np.roll(dither, -shift_b, axis=1)
+        self.result[:, :, 1] = dither
+        self.result[:, :, 2] = np.roll(dither, shift_r, axis=1)
 
         self.is_update_needed = False
 
