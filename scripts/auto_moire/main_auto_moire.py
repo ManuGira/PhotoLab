7ab00cbe78e1a03de270185c95bc4def33751400
Manu Gira <emmanuel.girardin@outlook.com>
2025-04-02 19:56:02 +0200

    MOD: create class for ModShif

--- scripts/auto_moire/main_auto_moire.py
+++ scripts/auto_moire/main_auto_moire.py
@@ -9,59 +9,70 @@ import numpy.typing as npt
 import photolab.utils as phutil
 
 from numba import njit, vectorize
+import time
 
 
-@njit()
-def accumulate_x(img, mod):
-    M, N = img.shape
-    for i in range(M):
-        for j in range(1, N):
-            pix = img[i, j]
-            pix_left = img[i, j - 1]
-            img[i, j] = (pix + pix_left) % mod
-@njit()
-def apply_pattern(M:int, N:int, pattern_width:int, shift_map, result):
-    depth = pattern_width//2
-    for i in range(M):
-        y = i
-        for j in range(N):
-            for k in range(pattern_width):
-                x = j * pattern_width + k
-                result[y, x] = ((k + shift_map[i, j]) % pattern_width) // depth
-                # np.roll(pattern, shift_map[i, j], axis=1)
+class ModShift:
+    @staticmethod
+    def warmup():
+        print("Start warmup")
+        img12 = np.array([[0, 0]], dtype=np.uint8)
+        img14 = np.array([[0, 0, 0, 0]], dtype=np.uint8)
+        ModShift.accumulate_x(img12, 1)
+        ModShift.apply_pattern(1, 2, 2, img12, img14)
+        print("warmup done")
 
-def apply(img, pattern_width) -> np.array:
-    depth = pattern_width // 2
-    shift_map = cv2.resize(img, None, None, fx=1 / pattern_width, fy=1, interpolation=cv2.INTER_AREA)
-    shift_map = (shift_map.astype(float) * depth / 255).astype(np.uint8)
-    accumulate_x(shift_map, pattern_width)
+    @staticmethod
+    @njit()
+    def accumulate_x(img, mod):
+        M, N = img.shape
+        for i in range(M):
+            for j in range(1, N):
+                pix = img[i, j]
+                pix_left = img[i, j - 1]
+                img[i, j] = (pix + pix_left) % mod
 
-    M, N = shift_map.shape
-    HEIGHT = M
-    WIDTH = N * pattern_width
+    @staticmethod
+    @njit()
+    def apply_pattern(M: int, N: int, pattern_width: int, shift_map, result):
+        depth = pattern_width // 2
+        for i in range(M):
+            y = i
+            for j in range(N):
+                for k in range(pattern_width):
+                    x = j * pattern_width + k
+                    result[y, x] = int(((k + shift_map[i, j]) % pattern_width) > depth)
 
-    # for j in range(1, N):
-    #     row = shift_map[:, j]
-    #     prev_row = shift_map[:, j - 1]
-    #
-    #     shift_map[:, j] = np.mod(row + prev_row, depth)
+    @staticmethod
+    def apply(img, pattern_width) -> np.array:
+        print("ModShift.apply()")
 
-    # accumulate_x(shift_map)
+        shift_map = cv2.resize(img, None, None, fx=1 / pattern_width, fy=1, interpolation=cv2.INTER_AREA)
+        M, N = shift_map.shape
 
-    pattern = np.zeros((1, pattern_width), dtype=np.uint8)
-    pattern[:, depth:] = 1
 
-    result = np.zeros((HEIGHT, WIDTH), dtype=np.uint8)
-    apply_pattern(M, N, pattern_width, shift_map, result)
+        # we need: white -> no shift, black -> big shift. So we must invert the image
+        shift_map = 255-shift_map
 
-    # result = np.zeros((HEIGHT, WIDTH), dtype=np.uint8)
-    # for i in range(M):
-    #     y = i
-    #     for j in range(N):
-    #         x = j * pattern_width
-    #         result[y:y + 1, x:x + pattern_width] = np.roll(pattern, shift_map[i, j], axis=1)
+        # randomize first row to break vertical lines
+        half = pattern_width // 2
+        random_row = np.random.randint(0, 256, M // half + 1)
+        random_row = random_row.repeat(half)[:M]
+        shift_map[:, 0] = random_row
 
-    return result
+        # convert to int. Shift map correspond to amount of shift (roll) that needs to be applied on the pattern [0, 0, 0, 1, 1, 1] comparing to neighbor pattern on the left
+        shift_map = (shift_map.astype(float) * pattern_width / 255).astype(np.uint8)
+
+        # the accumulate func propagates each shift to neighbor pixel on the right. The shift is not absolute and not anymore relative to neighbor
+        ModShift.accumulate_x(shift_map, pattern_width)
+
+        HEIGHT = M
+        WIDTH = N * pattern_width
+
+        result = np.zeros((HEIGHT, WIDTH), dtype=np.uint8)
+        ModShift.apply_pattern(M, N, pattern_width, shift_map, result)
+
+        return result
 
 
 @dataclasses.dataclass()
@@ -73,8 +84,8 @@ class Config:
 class AutoMoireGUI:
     @dataclasses.dataclass()
     class Controls:
-        shift: gbn.SliderWidget
         width: gbn.SliderWidget
+        shift: gbn.SliderWidget
 
         def config_factory(self):
             config = Config(
@@ -91,36 +102,46 @@ class AutoMoireGUI:
         self.win = gbn.create_window("Auto Moiré")
 
         self.controls = AutoMoireGUI.Controls(
-            self.win.create_slider("shift", range(-21, 21), self.set_update_flag, initial_index=21),
-            self.win.create_slider("pattern_width", range(2, 21, 2), self.set_update_flag, initial_index=5),
+            self.win.create_slider("pattern_width", range(2, 21, 2), self.set_prepare_flag, initial_index=5),
+            self.win.create_slider("shift", range(-41, 41), self.set_update_flag, initial_index=41),
         )
 
         self.win.create_button("save", self.on_clicksave)
+        self.prepare_result: npt.NDArray
         self.result: npt.NDArray
+        self.is_prepare_needed = True
         self.is_update_needed = True
+
+    def set_prepare_flag(self, *args, **kwargs):
+        self.is_prepare_needed = True
 
     def set_update_flag(self, *args, **kwargs):
         self.is_update_needed = True
 
+    def prepare(self):
+        config = self.controls.config_factory()
+        img = cv2.imread(self.img_path, cv2.IMREAD_GRAYSCALE)
+        self.prepare_result = ModShift.apply(img, config.width)
+
     def update(self):
         config = self.controls.config_factory()
 
-        img = cv2.imread(self.img_path, cv2.IMREAD_GRAYSCALE)
-        # img = np.clip(img.astype(int) + config.shift, 0, 255).astype(np.uint8)
-        img1 = apply(img, config.width)
+        img2 = np.roll(self.prepare_result, shift=config.shift, axis=1)
 
-        img2 = np.roll(img1, shift=config.shift, axis=1)
-
-        self.result = img1 * img2 * 255
+        self.result = self.prepare_result * img2 * 255
 
     def show(self):
         while True:
-            # self.is_update_needed = True
+            if self.is_prepare_needed:
+                self.prepare()
+                self.is_prepare_needed = False
+                self.is_update_needed = True
+
             if self.is_update_needed:
                 self.update()
-                self.win.imshow(self.result, mode=gbn.MODE.FIT, cv2_interpolation=cv2.INTER_LINEAR)
+                self.win.imshow(self.result, mode=gbn.MODE.FIT, cv2_interpolation=cv2.INTER_NEAREST)
                 self.is_update_needed = False
-            gbn.waitKeyEx(1)
+            gbn.waitKeyEx(30)
 
     def on_clicksave(self):
         # res = make_alpha(self.result, [0, 0, 0])
@@ -128,8 +149,11 @@ class AutoMoireGUI:
 
 
 def main():
-    AutoMoireGUI("../../images/octopus.webp").show()
+    # AutoMoireGUI("../../images/octopus.webp").show()
+    AutoMoireGUI("../../images/matcha dark.png").show()
+    # AutoMoireGUI("../../images/david_656x457.jpg").show()
 
 
 if __name__ == '__main__':
+    ModShift.warmup()
     main()
